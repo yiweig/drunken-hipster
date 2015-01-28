5ca5dcb35bb63d4598927c4324225b859859be04
Yiwei Gao <ygao56@emory.edu>
2015-01-27 22:27:50 -0500

    updated templates, 34:00 into the video

--- storytime/story/views.py
+++ storytime/story/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
+from .models import Line
 
 # Create your views here.
 def home(request):
-	return render_to_response("story/home.html", {'hello':"Hello World!"})
+	return render_to_response("story/home.html", {'lines': Line.objects.all() })
