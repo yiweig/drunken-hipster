188dc6a8d9fbbff3814adaf65d116be3f16da31e
Yiwei Gao <ygao56@emory.edu>
2015-01-27 22:16:19 -0500

    added base and home templates

--- storytime/story/views.py
+++ storytime/story/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response
 from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-	return HttpResponse("Hello world!")
+	return render_to_response("story/home.html", {'hello':"Hello World!"})
